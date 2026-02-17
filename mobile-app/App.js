0297ffc1dcc29c38868fbe72f03b695cff25e695
ivanotsky79-netizen <ivanotsky79@gmail.com>
2026-02-17 23:28:02 +0300

    Fix Missing import

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import HomeScreen from './src/screens/HomeScreen';
 import ScanScreen from './src/screens/ScanScreen';
