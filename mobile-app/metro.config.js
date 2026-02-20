5be15216c8858bb44cf6883d68e02aec3fb3e750
ivanotsky79-netizen <ivanotsky79@gmail.com>
2026-02-20 15:52:47 +0300

    Update metro.config.js

--- mobile-app/metro.config.js
+++ mobile-app/metro.config.js
@@ -2,6 +2,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Enable package exports for packages like axios
+config.resolver.unstable_enablePackageExports = true;
+
 // Add mp3 and other audio formats to asset extensions
 config.resolver.assetExts.push('mp3', 'wav', 'ogg', 'm4a');
 
